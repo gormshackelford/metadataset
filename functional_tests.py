27c9193237be74efb141cc1a22aa36bdbace9e27
gormshackelford <gorm.shackelford@gmail.com>
2017-12-03 16:21:26 +0000

    Added selenium

---
+++ functional_tests.py
@@ -0,0 +1,18 @@
+from selenium import webdriver
+import unittest
+
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_load_home(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('metadataset', self.browser.title)
+        self.fail('Finish the test!')
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
--- metadataset/settings.py
+++ metadataset/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config.SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
