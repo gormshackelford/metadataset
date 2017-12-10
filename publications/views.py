2f67ac94a53d755b5d1a0e72f80d2af1f1e84167
gormshackelford <gorm.shackelford@gmail.com>
2017-12-10 17:55:41 +0000

    Hello world

--- publications/views.py
+++ publications/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse('<html><title>metadataset</title></html>')
+    return HttpResponse('<html><title>metadataset</title><body>Hello world!</body></html>')
