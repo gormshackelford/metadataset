bd69aac5e105216b046608c8b04a098bc02eebc9
gormshackelford <gorm.shackelford@gmail.com>
2017-12-10 17:29:06 +0000

    Added createsu

--- metadataset/settings.py
+++ metadataset/settings.py
@@ -17,12 +17,10 @@ import json
 # Load the configuration file
 try:
     config_file = "config.json"
-    with open(config_file, 'r') as f:
-        config = json.load(f)
 except:
     config_file = "/tmp/config.json"
-    with open(config_file, 'r') as f:
-        config = json.load(f)
+with open(config_file, 'r') as f:
+    config = json.load(f)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -145,4 +143,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'www', 'static')
---
+++ publications/management/commands/createsu.py
@@ -0,0 +1,9 @@
+from django.core.management.base import BaseCommand, CommandError
+from django.contrib.auth.models import User
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        if not User.objects.filter(username="admin").exists():
+            User.objects.create_superuser("admin", "admin@admin.com", "admin")
+            self.stdout.write(self.style.SUCCESS('Successfully created new super user'))
