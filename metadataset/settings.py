f06895e37d8390900008ede772c2ac2a085631a4
gormshackelford <gorm.shackelford@gmail.com>
2018-02-05 14:14:25 +0000

    Changed database settings

--- metadataset/settings.py
+++ metadataset/settings.py
@@ -117,7 +117,10 @@ DATABASES = {
         'NAME': config['DB_NAME'],
         'USER': config['DB_USER'],
         'PASSWORD': config['DB_PASSWORD'],
-        'HOST': config['DB_HOST']
+        'HOST': config['DB_HOST'],
+        'OPTIONS': {
+            'init_command': 'SET sql_mode="STRICT_TRANS_TABLES", innodb_strict_mode=1',
+        }
     }
 }
 
