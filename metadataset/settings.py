7ade77fc3b148b59ac48ea858b39326bc4734b8e
gormshackelford <gorm.shackelford@gmail.com>
2019-05-15 17:08:48 +0100

    Changed DRF PAGE_SIZE to 250

--- metadataset/settings.py
+++ metadataset/settings.py
@@ -180,7 +180,7 @@ HAYSTACK_CONNECTIONS = {
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 25,
+    'PAGE_SIZE': 250,
 }
 
 # Internationalization
