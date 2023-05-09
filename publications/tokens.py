45c471339f310d45b013f6af54dc50f3b5612d8b
Alec <apc58@cam.ac.uk>
2023-05-09 12:58:03 +0100

    remove six from tokens

--- publications/tokens.py
+++ publications/tokens.py
@@ -1,5 +1,4 @@
 from django.contrib.auth.tokens import PasswordResetTokenGenerator
-from django.utils import six
 
 class AccountActivationTokenGenerator(PasswordResetTokenGenerator):
     def _make_hash_value(self, user, timestamp):
