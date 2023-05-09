310bb62a05e0c5344175c76f031bdae3bc895bf0
Alec <apc58@cam.ac.uk>
2023-05-09 13:22:03 +0100

    fix signup tokens error

--- publications/tokens.py
+++ publications/tokens.py
@@ -1,11 +1,10 @@
 from django.contrib.auth.tokens import PasswordResetTokenGenerator
-from six import python_2_unicode_compatible
 
 class AccountActivationTokenGenerator(PasswordResetTokenGenerator):
     def _make_hash_value(self, user, timestamp):
         return (
-            six.text_type(user.pk) + six.text_type(timestamp) +
-            six.text_type(user.profile.email_is_confirmed)
+            str(user.pk) + str(timestamp) +
+            str(user.profile.email_is_confirmed)
         )
 
 account_activation_token = AccountActivationTokenGenerator()
